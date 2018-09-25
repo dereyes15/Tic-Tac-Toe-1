dcc69e333c2ab024943c441d8824cba70c37cf38
learn <learn@learn06-2.local>
2018-09-25 15:08:41 -0700

    added comments to the Board.js file

--- src/Board.js
+++ src/Board.js
@@ -3,7 +3,6 @@ import React, {
 } from 'react';
 import './Board.css';
 import Square from './Square.js'
-// import WinCondition from './winCondition.js'
 
 class Board extends Component {
   constructor(props) {
@@ -14,11 +13,22 @@ class Board extends Component {
     }
   }
 
-  placeMarker = (index, currentPlayer) => {
+  //This function takes in two arguments - index and the marker of the current player
+      //This function is passed to the Square component (its child) as props
+      //The index is determined by the .map() in the render
+      //The currentMarker is determined by the marker of the current player in the Square component
+  placeMarker = (index, currentMarker) => {
+      //destructure the two states, squaresArray and counter, to interact with and update
       let { squaresArray, counter } = this.state
 
-      squaresArray[ index ] = currentPlayer
+      //place the current player's marker on the board
+      //it also add the current marker to the squaresArray
+      //this will display the marker of the current player on the board
+      squaresArray[ index ] = currentMarker
 
+      //updates the state squaresArray and counter
+        //sets this.state.squaresArray to the destructed squaresArray variable
+        //increases this.state.counter by one to determine whose turn it is
       this.setState({
           squaresArray: squaresArray,
           counter: counter + 1
@@ -27,15 +37,21 @@ class Board extends Component {
   }
 
 
-
+  //this function refreshes the page
+    //it is used mostly in the "New Game" Button and after a player has won
   refreshPage = () => {
     window.location.reload()
   }
 
   render() {
+    //destructures this.state.squaresArray and this.state.counter into variables
     let { squaresArray, counter } = this.state
+    //destructures the placeMarker() and refreshPage() function
     let { placeMarker, refreshPage } = this
 
+    //maps through squaresArray
+      //for every index of the squaresArray, render the Square component with the following props:
+        //the index of the squaresArray (to be used in placeMarker()), this.state.counter, squaresArray, placeMarker()
     let squares = this.state.squaresArray.map((square, index) => {
       return ( <
         Square
